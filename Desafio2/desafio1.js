2188d24d56f60542220cee0b6de47a108ac569a8
Nicole Costa <nicolecoosta@gmail.com>
2024-02-26 08:36:01 -0300

    Mensagem do commit

---
+++ Desafio2/desafio1.js
@@ -0,0 +1,16 @@
+//Desafio 01
+
+const numero1 = 10
+const numero2 = 6
+
+const soma = numero1 + numero2
+const subtração = numero1 - numero2
+const multiplicaçao = numero1 * numero2
+const divisao = numero1 / numero2
+
+const resultado = [ soma, subtração, multiplicaçao, divisao]
+
+console.log("Resultado da Soma é:", resultado[0])
+console.log("Resultado da Subtração é:", resultado[1])
+console.log("Resultado da Multiplicação é:", resultado[2])
+console.log("Resultado da Divisão é:", resultado[3])
